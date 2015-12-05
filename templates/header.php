d0209b4da75e23755834c7410485043a1da6241d
Gil Domingues <gmdz95@gmail.com>
2015-12-05 02:22:14 +0000

    Edited header. Buttons and dynamic resizing

--- templates/header.php
+++ templates/header.php
@@ -7,9 +7,9 @@
   </head>
   <body>
     <div id="floatingHeader">
-      <a href="./">Home</a>
-      <a href="./new_event.php">Create Event</a>
+      <a class="myButton" href="./">Home</a>
+      <a class="myButton" href="./new_event.php">Create Event</a>
       <input id="search" type="text" name="search" placeholder="Events & Users"/>
-      <?php echo '<a id="user" href="?user=' . $_SESSION['username'] . '">' . $_SESSION['username'] . '</a>'?>
-      <a href="database/action_logout.php">Logout</a>
+      <?php echo '<a id="user" class="myButton" href="?user=' . $_SESSION['username'] . '">' . $_SESSION['username'] . '</a>'?>
+      <a class="myButton" href="database/action_logout.php">Logout</a>
     </div>
