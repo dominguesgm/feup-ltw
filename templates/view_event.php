62548177f839ea8ec12c8b268b035006b9c35b02
Gil Domingues <gmdz95@gmail.com>
2015-12-06 23:41:34 +0000

    Comments are back to past events

--- templates/view_event.php
+++ templates/view_event.php
@@ -14,7 +14,7 @@
   </div>
   <div id="options">
     <?php
-        if($event['creator']==$_SESSION['username']) { ?>
+        if($event['creator']==$_SESSION['username'] && !hasEventHappened($_GET['event'])) { ?>
           <div class="warning" id="inviteWarning"></div>
           <button id="invite" data-event="<?=$event['id']?>" type="button">Invite</button>
           <br><br>
@@ -33,11 +33,11 @@
                     echo '<li data-user="' . $invites[$i]['username'] . '" data-event="' . $event['id'] . '"><a href="./?user=' . $invites[$i]['username'] . '">' . $invites[$i]['username'] . '</a> <img class="removeInvite" src="res/cross.png" width="8" height="8"></li>';
             }?>
           </ul>
-    <?php } else { if(!hasEventHappened($_GET['event'])) {
+    <?php } else {
         if(!isAttendingEvent($_SESSION['username'], $event['id'])) {?>
         <button id="attendance" type="button" onclick="attend('<?=$_SESSION['username']?>', <?=$event['id']?>)">Attend</button>
         <?php } else {?>
-        <button id="attendance" type="button" onclick="attend('<?=$_SESSION['username']?>', <?=$event['id']?>)">Do not attend</button> <?php } } }?>
+        <button id="attendance" type="button" onclick="attend('<?=$_SESSION['username']?>', <?=$event['id']?>)">Do not attend</button> <?php } }?>
         <h4 id="usersAttending">Going:</h4>
         <ul id="attendance">
           <button id="showAttendance" data-event="<?=$event['id']?>">Show attendance</button>
